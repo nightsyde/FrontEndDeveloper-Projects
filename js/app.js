5b80733d96aac9903fcd8bc8145bd8fa8db163f9
nightsyde <nightside@iobbow.org>
2020-05-12 13:51:57 -0400

    update: project 2 update
    Correct js pointer in index.html. Coding updates to app.js

--- js/app.js
+++ js/app.js
@@ -1,40 +1,54 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
-
+let navBar = document.getElementById('navbar__list');
+let bufferDocument = new DocumentFragment();
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
+function createNavBar(){
+  let navItemList = document.getElementsByClassName('landing__container');
+  for(navItem of navItemList){
+    let li = document.createElement('li'); /* create new <li> */
+    let newAnchor = document.createElement('a'); /* create new <a> */
+    newAnchor.href = '#' + document.navItem.name; /* set <a href=''> */
+    newAnchor.innerHTML = document.navItem.text; /* set link text */
+    li.appendChild(newAnchor); /* add <a> to <li> */
+    bufferDocument.appendChild(li); /* add <li> to DocumentFragment */
+  }
+}
+function changeFocus(){
 
-
+}
 
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
-
+createNavBar();
+navBar.appendChild(bufferDocument);
 
 // Add class 'active' to section when near top of viewport
 
@@ -45,13 +59,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
