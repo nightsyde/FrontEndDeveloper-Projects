b5f8986ca80a21f2e9b77952dd37d93b10bd4713
nightsyde <nightside@iobbow.org>
2020-05-18 10:40:04 -0400

    update: Project Update
    Fix click-to-focus. Fix scroll-to-focus.

--- js/app.js
+++ js/app.js
@@ -19,8 +19,8 @@
 */
 let navBar = document.getElementById('navbar__list'); // get navbar__list from index.html
 let bufferDocument = new DocumentFragment(); // create fragment as change buffer
-let activeLink = document.getElementsByTagName('a')[0]; // global for link item with class 'active_Selection'
-let activeSection = document.getElementsByTagName('section')[0]; // global for section item with class 'active_Selection'
+let activeSection = 'irma'; // global for section item with class 'active_Selection'
+let activeLink = 'bob'; // global for link item with class 'active_Selection'
 
 /**
  * End Global Variables
@@ -29,59 +29,68 @@ let activeSection = document.getElementsByTagName('section')[0]; // global for s
 */
 // create navigation bar from list of sections
 function createNavBar(){
-  const navItemList = document.getElementsByTagName('section');
+  const navItemList = document.getElementsByTagName('section'); // create list of sections
   for(const navItem of navItemList){
-    const id = navItem.getAttribute('id'); /* get id of section */
-    const navName = navItem.getAttribute('data-nav'); /* get data-nav of section */
-    const li = document.createElement('li'); /* create new <li> */
-    const newAnchor = document.createElement('a'); /* create new <a> */
-    newAnchor.setAttribute('id',`${id}NavLink`);
-    newAnchor.setAttribute('localName',id);
-    newAnchor.href = `#${id}` /* set <a href=''> */
-    newAnchor.innerHTML = navName; /* set link text */
-    li.appendChild(newAnchor); /* add <a> to <li> */
-    bufferDocument.appendChild(li); /* add <li> to DocumentFragment */
+    const id = navItem.getAttribute('id'); // get id of section
+    const navName = navItem.getAttribute('data-nav'); // get data-nav of section
+    const li = document.createElement('li'); // create new <li>
+    const newAnchor = document.createElement('a'); // create new <a>
+    newAnchor.setAttribute('id',`${id}NavLink`); // set 'id' attribute of new <a>
+    newAnchor.setAttribute('localName',id); // set 'localName' attribute of new <a>
+    newAnchor.href = `#${id}`; // set <a href=''>
+    newAnchor.innerHTML = navName; // set link text
+        li.appendChild(newAnchor); // add <a> to <li>
+    bufferDocument.appendChild(li); // add <li> to DocumentFragment
   }
+}
+// set initial values for active element variables
+function setActive() {
+  activeLink = document.getElementsByTagName('a')[0]; // set to first <a>
+  activeSection = document.getElementsByTagName('section')[0]; // set to first <section>
 }
 // identify active_Selection elements
 function findActive() {
-  const oldActives = document.getElementsByClassName('active_Selection');
-  activeSection = document.getElementsByClassName('active_Selection')[1];
-  activeLink = document.getElementsByClassName('active_Selection')[0];
+  const oldActives = document.getElementsByClassName('active_Selection'); // get list of active elements
+  activeSection = document.getElementsByClassName('active_Selection')[1]; // set active section to second element in list
+  activeLink = document.getElementsByClassName('active_Selection')[0]; // set active link to first element of list
 }
-
 // remove active_Selection
 function removeFocus() {
-  activeLink.setAttribute('class','');
-  activeSection.setAttribute('class','');
+  activeLink.setAttribute('class',''); // remove all 'class' attributes from active link
+  activeSection.setAttribute('class',''); // remove all 'class' attributes from active section
 }
-
 // Add class 'active' to section when near top of viewport
-function changeFocus(newActive,newLink){
+function changeFocus(){
 //  removeFocus();
-  newLink.setAttribute('class','active_Selection');
-  newActive.setAttribute('class','active_Selection');
+  activeLink.setAttribute('class','active_Selection'); // assign active link to 'active_Selection' 'class'
+  activeSection.setAttribute('class','active_Selection'); // assign active section to 'active_Selection' 'class'
 }
-
+function isInView(element) {
+  let viewportTop = window.visualViewport.pageTop; // get current top value of viewport
+  let viewportMid = (window.visualViewport.height / 2); // compute midpoint of viewport
+  let viewportBottom = window.visualViewport.height + viewportTop; // compute bottom of viewport
+  let bounding = element.getBoundingClientRect(); // get values of section bountries
+  if(bounding.top >= 0) { // is top of section below the top of the viewport?
+    if(bounding.top <= viewportMid) { // is top of section above the midpoint of the viewport?
+      return true;
+    } else if(bounding.bottom < viewportMid){ // is the bottom of the section above the midpoint of the viewport?
+      return false;
+    } else {
+      return false;
+    }
+  }
+}
 // compare pageTop to top of each section
 function get(last_known_scroll_position) {
   const sectionList = document.getElementsByTagName('section'); // get list of sections
-  let viewportTop = window.visualViewport.pageTop; // get current top value of viewport
-  let viewportMid = (window.visualViewport.height / 2) + viewportTop;
-  let viewportBottom = window.visualViewport.height + viewportTop;
-  for(sectionItem of sectionList){ // compare top value of each section to viewportTop
-    let bounding = sectionItem.getBoundingClientRect();
-    let variance = viewportTop - bounding.top;
+  for(sectionItem of sectionList){
     let sectionItemID = sectionItem.id;
     let sectionItemLink = document.getElementById(`${sectionItemID}NavLink`);
-    if(viewportTop < bounding.top && bounding.top < viewportMid) {
-//      console.log(`${bounding}`)
-//      console.log(sectionItem,sectionItemLink);
-      changeFocus(sectionItem,sectionItemLink);
-    } else if (viewportMid < bounding.bottom) {
-      removeFocus();
-    } else if (bounding.top < viewportMid) {
-      removeFocus();
+    if(isInView(sectionItem)) { // is the section visible?
+      removeFocus(); 
+      activeLink = sectionItemLink;
+      activeSection = sectionItem;
+      changeFocus();
     }
   }
 }
@@ -95,20 +104,21 @@ function get(last_known_scroll_position) {
 // build the nav
 createNavBar();
 navBar.appendChild(bufferDocument);
+setActive();
+
 
 
 // Scroll to anchor ID using scrollTO event
 function linkClicked(evt) { // function to listen for when a link is clicked
-  const chosenElement = document.getElementById(evt.target.id);
-  //console.log(chosenElement + " clicked");
-  const chosenLink = chosenElement.getAttribute('localName');
-  //console.log(chosenLink);
-  const chosenAnchor = document.getElementById(chosenLink);
-  //console.log(chosenAnchor);
-  chosenAnchor.scrollTo({behavior: 'smooth'});
-  findActive();
   removeFocus();
-  changeFocus(chosenAnchor,chosenElement);
+  activeLink = document.getElementById(evt.target.id);
+  //console.log(chosenElement + " clicked");
+  const chosenLink = activeLink.getAttribute('localName');
+  //console.log(chosenLink);
+  activeSection = document.getElementById(chosenLink);
+  //console.log(chosenAnchor);
+  activeSection.scrollTo({behavior: 'smooth'});
+  changeFocus();
 
 }
 
@@ -125,7 +135,7 @@ function linkClicked(evt) { // function to listen for when a link is clicked
 navBar.addEventListener('click', linkClicked);
 
 // Set sections as active
-/* window.addEventListener('scroll', function(e) {
+ window.addEventListener('scroll', function(e) {
   let last_known_scroll_position = window.scrollY;
   get(last_known_scroll_position);
-}); */
+});
