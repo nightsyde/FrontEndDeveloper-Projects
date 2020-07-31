bada7acf945224c6e3d5ee88012a11c9ac5d74fd
nightsyde <nightside@iobbow.org>
2020-07-31 14:17:09 -0400

    update: Project 4 v1.1
    remove 'clean webpack' plugin from prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -62,15 +62,6 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: ".\\src\\client\\views\\index.html",
     }),
-    new CleanWebpackPlugin({
-        // Simulate the removal of files
-        dry: true,
-        // Write Logs to Console
-        verbose: true,
-        // Automatically remove all unused webpack assets on rebuild
-        cleanStaleWebpackAssets: true,
-        protectWebpackAssets: false
-    }),
     new MiniCssExtractPlugin({ filename: "[name].css" }),
     new WorkboxPlugin.GenerateSW({
       // Do not precache images
